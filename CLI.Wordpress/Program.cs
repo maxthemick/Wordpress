e34cbc5c950959656975aaa736d5f8b871605004
Bigsh <Bigsh@MAX_LAPTOP>
2025-09-14 22:01:12 -0400

    Add project files.

---
+++ CLI.Wordpress/Program.cs
@@ -0,0 +1,59 @@
+﻿using Library.WordPress.Models;
+using System;
+
+namespace CLI.WordPress
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to WordPress!");
+            List<Blog?> blogPosts = new List<Blog?>();
+            bool cont = true;
+
+            do
+            {
+                Console.WriteLine("C. Create a Blog Post");
+                Console.WriteLine("R. List all Blog Posts");
+                Console.WriteLine("U. Update a Blog Post");
+                Console.WriteLine("D. Delete a Blog Post");
+                Console.WriteLine("Q. Quit");
+
+                var userChoice = Console.ReadLine();
+
+                switch (userChoice)
+                {
+                    case "C":
+                    case "c":
+                        var blog = new Blog();
+                        blog.Title = Console.ReadLine();
+                        blog.Content = Console.ReadLine();
+                        blogPosts.Add(blog);
+                        break;
+                    case "R":
+                    case "r":
+                        foreach (var b in blogPosts)
+                        {
+                            Console.WriteLine(b);
+                        }
+                        break;
+                    case "U":
+                    case "u":
+                        break;
+                    case "D":
+                    case "d":
+                        break;
+                    case "Q":
+                    case "q":
+                        cont = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Command");
+                        break;
+                }
+
+            } while (cont);
+        }
+    }
+}
---
+++ Library.WordPress/Models/Blog.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.WordPress.Models
+{
+    public class Blog
+    {
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+
+        public int Id { get; set; }
+
+
+    }
+}
